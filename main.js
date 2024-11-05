43b062b1ceec016edcdb20cdfcf9016101f033b8
FaizMans <125295975+faeiiz@users.noreply.github.com>
2024-11-05 13:55:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// import * as THREE from "three";
-// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js'
 
